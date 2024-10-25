599d3ba9df7bf0d50241e57e8243ec8bfe3b9660
Tim Ruscica <35348871+techwithtim@users.noreply.github.com>
2024-10-25 18:02:40 +0400

    Update 3-5.py

--- 3-5.py
+++ 3-5.py
@@ -4,14 +4,13 @@ from diffusers import  StableDiffusion3Pipeline
 large_model = "stabilityai/stable-diffusion-3.5-large"
 
 pipe = StableDiffusion3Pipeline.from_pretrained(large_model, torch_dtype=torch.bfloat16)
+pipe.enable_attention_slicing()
 pipe = pipe.to("cuda")
 
-prompts = [
-    "a programmer touching grass",
-]
+prompt = "a programmer touching grass"
 
 results = pipe(
-    prompts,
+    prompt,
     num_inference_steps=20,
     guidance_scale=3.5,
     height=512,
